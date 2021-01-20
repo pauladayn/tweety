0a1b044f3f47b998dcebb42b642b94bf98020898
ManuJ90 <juan.scagni@gmail.com>
2021-01-20 18:53:02 -0300

    Update

---
+++ tweetBank.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+
+let data = [];
+
+function add (name, content) {
+    data.push({ name: name, content: content });
+  }
+  function list () {
+    return _.cloneDeep(data);
+  }
+  function find (properties) {
+    return _.cloneDeep(_.filter(data, properties));
+  }
+
+  module.exports = { add: add, list: list, find: find };
